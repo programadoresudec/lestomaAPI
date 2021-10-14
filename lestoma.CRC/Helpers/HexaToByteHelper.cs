155944833b105eb417b044b250a598acc49bf2a7
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-10-13 22:43:43 -0500

    CRC modbus16 @Anderson0816  @ranasofia  y @sebsnaranjo

---
+++ lestoma.CRC/Helpers/HexaToByteHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace lestoma.CRC.Helpers
+{
+    public class HexaToByteHelper
+    {
+        public static byte[] StringToByteArray(string hex)
+        {
+            hex = hex.Replace(" ", "");
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+    }
+}
