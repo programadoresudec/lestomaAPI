f332a9a8ef8b450dc9db5f4e0803a6e918eab6b6
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2021-10-21 14:58:44 -0500

    controller crc

---
+++ lestoma.Api/Controllers/CalculosCRCController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using lestoma.CommonUtils.DTOs;
+using lestoma.CRC.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lestoma.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculosCRCController : BaseController
+    {
+        public CalculosCRCController(IMapper mapper) : base(mapper)
+        {
+
+        }
+
+        [HttpPost("CRCModbus")]
+        public async Task<IActionResult> CalcularCRCModbus(string hexadecimal)
+        {
+            var crc = CalcularCRCHelper.CalculateCrc16Modbus(hexadecimal);
+            var response = new Response
+            {
+                Data = crc,
+                Mensaje = "crc"
+            };
+            //byte[] bytesMOdbus = CalcularCRCHelper.CalculateCrc16Modbus(Trama);
+
+            //var resultado = new List<byte>();
+            //resultado.Add(bytesMOdbus.ElementAt(1));
+            //resultado.Add(bytesMOdbus.ElementAt(0));
+
+            //string hexa = HexaToByteHelper.ByteArrayToHexString(resultado.ToArray());
+            return Created(string.Empty, response);
+        }
+    }
+}
