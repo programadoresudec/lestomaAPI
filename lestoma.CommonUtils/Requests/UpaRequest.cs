abf8008d7412cd2730d5b34da523ab27814188b4
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2023-03-29 18:23:00 -0500

    upa crear bug

--- lestoma.CommonUtils/Requests/UpaRequest.cs
+++ lestoma.CommonUtils/Requests/UpaRequest.cs
@@ -8,27 +8,29 @@ namespace lestoma.CommonUtils.Requests
     public class UpaRequest : IId
     {
         public Guid Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El nombre es requerido.")]
         public string Nombre { get; set; }
-        [Required]
+        [Required(ErrorMessage = "La descripción es requerida.")]
+        [StringLength(5000, MinimumLength = 6, ErrorMessage = "Minimo 10 caracteres y maximo 5000 caracteres.")]
         public string Descripcion { get; set; }
-        [Required]
+        [Required(ErrorMessage = "cantidad requerida.")]
+        [Range(1, 50, ErrorMessage = "El valor debe ser mayor que 0.")]
         public short CantidadActividades { get; set; }
-        [Required]
-        public ICollection<ProtocoloRequest> ProtocolosCOM { get; set; }
+        [Required(ErrorMessage = "Debe insertar 1 o más protocolos.")]
+        public IList<ProtocoloRequest> ProtocolosCOM { get; set; }
 
     }
     public class UpaEditRequest : IId
     {
-        [Required(ErrorMessage = "El id de la upa es requerido")]
+        [Required(ErrorMessage = "El id de la upa es requerido.")]
         public Guid Id { get; set; }
-        [Required(ErrorMessage = "El nombre es requerido")]
+        [Required(ErrorMessage = "El nombre es requerido.")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "La descripción es requerida.")]
-        [StringLength(5000, MinimumLength = 6, ErrorMessage = "Minimo 10 caracteres y maximo 5000 caracteres")]
+        [StringLength(5000, MinimumLength = 6, ErrorMessage = "Minimo 10 caracteres y maximo 5000 caracteres.")]
         public string Descripcion { get; set; }
         [Required(ErrorMessage = "cantidad requerida.")]
-        [Range(1, 50, ErrorMessage = "El valor debe ser mayor que 0")]
+        [Range(1, 50, ErrorMessage = "El valor debe ser mayor que 0.")]
         public short CantidadActividades { get; set; }
     }
 }
